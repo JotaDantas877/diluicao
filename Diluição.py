22ee71053ba7db870d4938b966c105c2c55c9f2c
Jota Dantas <77057996+JotaDantas877@users.noreply.github.com>
2022-04-11 11:42:44 -0300

    Diluição bio

---
+++ Diluição.py
@@ -0,0 +1,13 @@
+
+n1 = float(input("insira o valor da concentração inicial: "))
+n2 = float(input("Insira o valor da concentração final: "))
+n3= float(input("Insira o valor do que você precisa: "))
+
+
+x = n3 * n2 /n1
+
+
+print("Obrigado, Vamos calcular agora...")
+print(f" pronto o que você precisa é de {x}ml" )
+
+
